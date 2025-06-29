2539f58f3cc918a8240b4fd803caf562dcc561b3
Bilunzzz <hainakamakmur06@gmail.com>
2025-06-29 17:32:47 +0700

    Fix: Update base path in Vite configuration for consistent deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Sistem-Manajemen-Akademik/", // Nama repository GitHub Anda
+  base: "/", // Nama repository GitHub Anda
   resolve: {
     alias: {
       "@": "/src",
